27ea652185f12447357133aebfaa31b7a290a478
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-20 13:46:37 +0200

    feat: Set up a basic schema and implement raw queries

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client';
+import db from './db/db.service';
+import { Expense } from './db/models';
 
 dotenv.config();
 
-const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -18,109 +18,90 @@ app.get('/ping', (req: Request, res: Response) => {
   res.json({ message: 'pong' });
 });
 
-app.post('/expenses', async (req: Request, res: Response) => {
+app.post('/expenses', (req: Request<{}, {}, Expense>, res: Response) => {
   const { name, amount, currency, category } = req.body;
   const date = new Date().toISOString();
-
-  try {
-    const expense = await prisma.expense.create({
-      data: {
-        name,
-        amount,
-        currency,
-        category,
-        date
-      }
-    });
-
-    res.status(201).json({
-      message: 'Expense record has been successfully created.',
-      expense
-    });
-  } catch (error) {
-    console.error('Error fetching expense:', error);
-    res.status(500).json({ message: 'Error creating expense record.' });
-  }
+  const insert = db.prepare(`
+    INSERT INTO expenses (name, amount, currency, category, date)
+    VALUES (?, ?, ?, ?, ?)
+  `);
+  const result = insert.run(name, amount, currency, category, date);
+  res.status(201).json({
+    message: 'Expense record has been successfully created.',
+    expense: {
+      id: result.lastInsertRowid,
+      name,
+      amount,
+      currency,
+      category,
+      date
+    }
+  });
 });
 
 app.get('/expenses', async (req: Request, res: Response) => {
-  try {
-    const expenses = await prisma.expense.findMany();
-    res.status(200).json(expenses);
-  } catch (error) {
-    console.error('Error fetching expense:', error);
-    res.status(500).json({ message: 'Error fetching expenses.' });
-  }
+  const selectQuery = `SELECT * FROM expenses;`;
+  const expenses = db.prepare(selectQuery).all();
+  res.status(200).json(expenses);
 });
 
-app.get('/expenses/:id', async (req: Request, res: Response) => {
+app.get('/expenses/:id', (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const expense = await prisma.expense.findUnique({
-      where: { id: parseInt(id) }
-    });
+  const selectQuery = `SELECT * FROM expenses WHERE id = ?;`;
+  const expense = db.prepare(selectQuery).get(id);
 
-    if (expense) {
-      res.status(200).json(expense);
-    } else {
-      res.status(404).json({ message: 'Expense record not found.' });
-    }
-  } catch (error) {
-    console.error('Error fetching expense:', error);
-    res.status(500).json({ message: 'Error fetching expense.' });
-  }
-});
-
-app.put('/expenses/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { name, amount, currency, category } = req.body;
-  const date = new Date().toISOString();
-
-  try {
-    const updatedExpense = await prisma.expense.update({
-      where: { id: parseInt(id) },
-      data: {
-        name,
-        amount,
-        currency,
-        category,
-        date
-      }
-    });
-
-    res.status(200).json({
-      message: 'Expense record has been successfully updated.',
-      expense: updatedExpense
-    });
-  } catch (error) {
-    console.error('Error fetching expense:', error);
+  if (expense) {
+    res.status(200).json(expense);
+  } else {
     res.status(404).json({ message: 'Expense record not found.' });
   }
 });
 
-app.delete('/expenses/:id', async (req: Request, res: Response) => {
+app.put('/expenses/:id', (req: Request, res: Response) => {
   const { id } = req.params;
+  const { name, amount, currency, category } = req.body;
+  const date = new Date().toISOString();
 
-  try {
-    const deletedExpense = await prisma.expense.delete({
-      where: { id: parseInt(id) }
-    });
+  const updateQuery = `
+    UPDATE expenses 
+    SET name = ?, amount = ?, currency = ?, category = ?, date = ? 
+    WHERE id = ?;
+  `;
+  const result = db
+    .prepare(updateQuery)
+    .run(name, amount, currency, category, date, id);
 
+  if (result.changes > 0) {
     res.status(200).json({
-      message: 'Expense record has been successfully deleted.',
-      expense: deletedExpense
+      message: 'Expense record has been successfully updated.',
+      expense: {
+        id,
+        name,
+        amount,
+        currency,
+        category,
+        date
+      }
     });
-  } catch (error) {
-    console.error('Error fetching expense:', error);
+  } else {
+    res.status(404).json({ message: 'Expense record not found.' });
+  }
+});
+
+app.delete('/expenses/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const deleteQuery = `DELETE FROM expenses WHERE id = ?;`;
+  const result = db.prepare(deleteQuery).run(id);
+
+  if (result.changes > 0) {
+    res
+      .status(200)
+      .json({ message: 'Expense record has been successfully deleted.' });
+  } else {
     res.status(404).json({ message: 'Expense record not found.' });
   }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
-
-process.on('SIGINT', async () => {
-  await prisma.$disconnect();
-  process.exit(0);
 });
