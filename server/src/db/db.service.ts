d3baf5dae2ec4121186d0157df098733d854fe77
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-20 15:29:04 +0200

    refactor: code structure has been updated

--- server/src/db/db.service.ts
+++ server/src/db/db.service.ts
@@ -1,21 +1,3 @@
-import Database from 'better-sqlite3';
+import { PrismaClient } from '@prisma/client';
 
-const db = new Database('app.db');
-
-const initializeDatabase = () => {
-  const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS expenses (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    amount REAL NOT NULL,
-    currency TEXT NOT NULL,
-    category TEXT NOT NULL,
-    date DATETIME DEFAULT CURRENT_TIMESTAMP
-  );
-`;
-  db.prepare(createTableQuery).run();
-};
-
-initializeDatabase();
-
-export default db;
+export const prisma = new PrismaClient();
