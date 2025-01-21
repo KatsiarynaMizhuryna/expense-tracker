c30a585a6d8646a3232e085d5df5e2589e9f5dac
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-21 17:04:30 +0200

    refactor: extract validation messages into constants

--- server/src/helpers/middlewares/errorHandler.ts
+++ server/src/helpers/middlewares/errorHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { ERROR_INTERNAL_SERVER } from '../../constants/messages';
 
 export const errorHandler = (
   err: any,
@@ -9,5 +10,5 @@ export const errorHandler = (
   console.error(err);
   res
     .status(err.status || 500)
-    .json({ message: err.message || 'Internal Server Error' });
+    .json({ message: err.message || ERROR_INTERNAL_SERVER });
 };
