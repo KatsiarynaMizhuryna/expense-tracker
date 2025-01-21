9f5523879325df99b3299590f656a8ae6e9099e4
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-21 17:02:57 +0200

    refactor: extract validation messages into constants

--- server/src/constants/messages.ts
+++ server/src/constants/messages.ts
@@ -4,3 +4,9 @@ export const SUCCESS_DELETE_EXPENSE = 'Expense deleted successfully';
 export const ERROR_INVALID_INPUT = 'Invalid input provided';
 export const ERROR_INTERNAL_SERVER = 'Internal server error occurred';
 export const ERROR_NOT_FOUND = 'Resource not found';
+export const VALIDATION_MESSAGES = {
+  NAME_REQUIRED: 'Name is required',
+  AMOUNT_INVALID: 'Amount must be a number',
+  CURRENCY_INVALID: 'Currency must be 3 characters',
+  CATEGORY_REQUIRED: 'Category is required'
+};
--- server/src/helpers/middlewares/validator.ts
+++ server/src/helpers/middlewares/validator.ts
@@ -1,11 +1,21 @@
 import { body } from 'express-validator';
+import { VALIDATION_MESSAGES } from '../../constants/messages';
 
 export const createExpenseSchema = [
-  body('name').isString().withMessage('Name is required').notEmpty(),
-  body('amount').isNumeric().withMessage('Amount must be a number').notEmpty(),
+  body('name')
+    .isString()
+    .withMessage(VALIDATION_MESSAGES.NAME_REQUIRED)
+    .notEmpty(),
+  body('amount')
+    .isNumeric()
+    .withMessage(VALIDATION_MESSAGES.AMOUNT_INVALID)
+    .notEmpty(),
   body('currency')
     .isLength({ min: 3, max: 3 })
-    .withMessage('Currency must be 3 characters')
+    .withMessage(VALIDATION_MESSAGES.CURRENCY_INVALID)
     .notEmpty(),
-  body('category').isString().withMessage('Category is required').notEmpty()
+  body('category')
+    .isString()
+    .withMessage(VALIDATION_MESSAGES.CATEGORY_REQUIRED)
+    .notEmpty()
 ];
