5d601643e197e5b952bb0f81b85ef7ccd5d2bbbd
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-21 23:06:34 +0200

    feat: constants for errors and statuses added

--- server/src/constants/messages.ts
+++ server/src/constants/messages.ts
@@ -2,7 +2,12 @@ export const SUCCESS_CREATE_EXPENSE = 'Expense created successfully';
 export const SUCCESS_UPDATE_EXPENSE = 'Expense updated successfully';
 export const SUCCESS_DELETE_EXPENSE = 'Expense deleted successfully';
 export const ERROR_INVALID_INPUT = 'Invalid input provided';
+export const ERROR_CREATE_EXPENSE = 'Error during expense creation';
+export const ERROR_UPDATE_EXPENSE = 'Error during expense update';
+export const ERROR_DELETE_EXPENSE = 'Error during expense deletion';
+export const ERROR_FETCH_EXPENSE = 'Error fetching expenses';
 export const ERROR_INTERNAL_SERVER = 'Internal server error occurred';
+export const ERROR_DATABASE_SERVER = 'A database error occurred.';
 export const ERROR_NOT_FOUND = 'Resource not found';
 export const VALIDATION_MESSAGES = {
   NAME_REQUIRED: 'Name is required',
