49ae3d3b4e7f2ed9ca7df0cffecf5bccc158c9f7
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-21 23:09:24 +0200

    feat: integrate prismaExceptionMiddleware into server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import expensesRouter from './expenses/expenses.controller';
 import { errorHandler } from './helpers/middlewares/errorHandler';
+import { prismaExceptionMiddleware } from './helpers/Exception';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/expenses', expensesRouter);
+
+app.use(prismaExceptionMiddleware);
 
 app.use(errorHandler);
 
