a927de74aef6979b6ca58efd6e6560acb79b7d2d
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-22 17:42:24 +0200

    feat: logo created

--- client/src/components/primitive/Logo.tsx
+++ client/src/components/primitive/Logo.tsx
@@ -1,5 +1,13 @@
-const Logo = () => {
-  return <div></div>;
+import React from 'react';
+import styles from './Logo.module.scss';
+
+interface LogoProps {
+  src: string;
+  alt: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ src, alt }) => {
+  return <img src={src} alt={alt} className={styles.logo} />;
 };
 
 export default Logo;
