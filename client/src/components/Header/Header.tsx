8cd3cf4958eae9f2c4fc64c679c2ebf7ca6ea591
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2025-01-31 15:27:39 +0200

    feat: create header with logo

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,6 +1,13 @@
+import Logo from '../Logo/Logo';
 import styles from './Header.module.scss';
+import logo from '../../assets/logo.png';
+
 function Header() {
-  return <div className={styles.header}></div>;
+  return (
+    <div className={styles.header}>
+      <Logo src={logo} alt={'logo'} />
+    </div>
+  );
 }
 
 export default Header;
